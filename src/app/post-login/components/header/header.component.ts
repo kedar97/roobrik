6754ec2d147e702891a347cb730cba3249d5f33e
rushitvora-93 <rushit.dev93@gmail.com>
2023-10-05 13:10:34 +0530

    Refactor ui code

--- src/app/post-login/components/header/header.component.ts
+++ src/app/post-login/components/header/header.component.ts
@@ -9,67 +9,43 @@ import {
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  reports: boolean = false;
-  infoBtn: boolean = false;
-  profileBtn: boolean = false;
+  showReportMenu: boolean = false;
+  showInfoPopup: boolean = false;
+  showProfileMenu: boolean = false;
 
   constructor() {}
 
   showReportOptions(event: Event) {
-    this.reports = !this.reports;
-    this.infoBtn = this.profileBtn = false;
+    this.showReportMenu = !this.showReportMenu;
+    this.showInfoPopup = this.showProfileMenu = false;
     event.stopPropagation();
   }
 
   showInfoBtn(event: Event) {
-    this.infoBtn = !this.infoBtn;
-    this.reports = this.profileBtn = false;
+    this.showInfoPopup = !this.showInfoPopup;
+    this.showReportMenu = this.showProfileMenu = false;
     event.stopPropagation();
   }
 
   showProfileBtn(event: Event) {
-    this.profileBtn = !this.profileBtn;
-    this.reports = this.infoBtn = false;
+    this.showProfileMenu = !this.showProfileMenu;
+    this.showInfoPopup = this.showReportMenu = false;
     event.stopPropagation();
   }
 
   @HostListener('document:click', ['$event'])
   onClickOutside(event: Event) {
-    if (this.reports) {
-      const popupElement = document.querySelector('.popup-reports');
-      const buttonElement = document.querySelector('.link-reports');
+    const popupElement = document.querySelector('.popup');
+      const buttonElement = document.querySelector('.popup-btn');
       if (
         popupElement &&
         buttonElement &&
         !popupElement.contains(event.target as Node) &&
         !buttonElement.contains(event.target as Node)
       ) {
-        this.reports = false;
+        this.showReportMenu = this.showProfileMenu = this.showInfoPopup = false;
+
+
       }
-    }
-    if (this.infoBtn) {
-      const popupElement = document.querySelector('.popup-info');
-      const buttonElement = document.querySelector('.info-btn');
-      if (
-        popupElement &&
-        buttonElement &&
-        !popupElement.contains(event.target as Node) &&
-        !buttonElement.contains(event.target as Node)
-      ) {
-        this.infoBtn = false;
-      }
-    }
-    if (this.profileBtn) {
-      const popupElement = document.querySelector('.popup-profile');
-      const buttonElement = document.querySelector('.action-btn');
-      if (
-        popupElement &&
-        buttonElement &&
-        !popupElement.contains(event.target as Node) &&
-        !buttonElement.contains(event.target as Node)
-      ) {
-        this.profileBtn = false;
-      }
-    }
   }
 }
